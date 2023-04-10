c1a8ed03cd9e176977b884f841805237017a8cb5
nazekov <nazekov.ul@gmail.com>
2023-04-10 15:44:50 +0600

    added run()

--- src/main/java/com/example/money_transfer/MoneyTransferApplication.java
+++ src/main/java/com/example/money_transfer/MoneyTransferApplication.java
@@ -1,12 +1,26 @@
 package com.example.money_transfer;
 
+import com.example.money_transfer.model.Cashbox;
+import com.example.money_transfer.service.CashboxService;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class MoneyTransferApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(MoneyTransferApplication.class, args);
+    }
+
+    //@Bean
+    public CommandLineRunner run(CashboxService cashboxService) {
+        return args -> {
+            for (int i = 0; i < 2; i++) {
+                Cashbox cashbox = new Cashbox();
+                cashboxService.save(cashbox);
+            }
+        };
     }
 }
