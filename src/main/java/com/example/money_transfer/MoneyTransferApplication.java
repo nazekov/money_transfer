bdb647a5a6d72bf6d42644da1f2e4b6835befa60
nazekov <nazekov.ul@gmail.com>
2023-04-10 13:47:52 +0600

    initial commit

---
+++ src/main/java/com/example/money_transfer/MoneyTransferApplication.java
@@ -0,0 +1,12 @@
+package com.example.money_transfer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoneyTransferApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MoneyTransferApplication.class, args);
+    }
+}
---
+++ src/test/java/com/example/money_transfer/MoneyTransferApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.money_transfer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoneyTransferApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
